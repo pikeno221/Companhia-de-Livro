c904d9f9ffe646a75f224e4593a83c858cf3305e
pikeno221 <gabriellgomesbh@gmail.com>
2019-11-17 15:43:09 -0300

    redirecionamento com estado na classe login

--- src/admin/Login.js
+++ src/admin/Login.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-
-import {auth} from './../firebase-config'
+import React, { Component } from 'react'
+import {Redirect} from 'react-router-dom'
+import { auth } from './../firebase-config'
 
 class Login extends Component {
     constructor(props) {
@@ -9,39 +9,50 @@ class Login extends Component {
         this.email = null
         this.senha = null
 
+        this.state = {
+            estaAutenticado: false,
+            estaLogando: true,
+            erro: false
+        }
+
         this.autenticaUsuario = this.autenticaUsuario.bind(this)
     }
 
     autenticaUsuario() {
-        console.log(this.email.value)
-        console.log(this.senha.value)
         auth.signInWithEmailAndPassword(this.email.value, this.senha.value)
-        .then(user => {
-            console.log('Usuario Logado -> ', user)
-        })
-        .catch(err => {
-            console.log('Error -> ', err)
-        })
+            .then(user => {
+                console.log('Usuario Logado -> ', user)
+                this.setState({ estaAutenticado: true, estaLogando: false, erro: false })
+            })
+            .catch(err => {
+                this.setState({ erro: true, estaLogando:true, estaAutenticado: false})
+                console.log('Error -> ', err)
+            })
     }
 
 
     render() {
+        if (this.state.estaAutenticado) {
+            return <Redirect to='/admin' />
+
+        }
         return (
             <div style={{ padding: '120px' }} >
 
-                    <div className="form-group">
-                        <label htmlFor="exampleInputEmail1">Email</label>
-                        <input type="email" name="email" ref={ref => this.email = ref} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputPassword1">Senha</label>
-                        <input type="password" name="senha" ref={ref => this.senha = ref} className="form-control" id="exampleInputPassword1" placeholder="Senha" />
-                    </div>
-                    <div className="form-group form-check">
-                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                        <label className="form-check-label" htmlFor="exampleCheck1">Acessar</label>
-                    </div>
-                    <button type="button" className="btn btn-primary" onClick={this.autenticaUsuario}>Enviar</button>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Email</label>
+                    <input type="email" name="email" ref={ref => this.email = ref} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputPassword1">Senha</label>
+                    <input type="password" name="senha" ref={ref => this.senha = ref} className="form-control" id="exampleInputPassword1" placeholder="Senha" />
+                    {this.state.erro && <small id="errorCredentials" className="form-text text-muted">Usuario e/ou Senha incorretos</small>}
+                </div>
+                <div className="form-group form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                    <label className="form-check-label" htmlFor="exampleCheck1">Acessar</label>
+                </div>
+                <button type="button" disabled={!this.state.estaLogando} className="btn btn-primary" onClick={this.autenticaUsuario}>Enviar</button>
 
             </div>
         )
